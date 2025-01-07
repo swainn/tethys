bcd9f0c6452fcc4c98d27e39cd7d76c507868576
Nathan Swain <nswain@aquaveo.com>
2025-01-07 10:56:33 -0700

    Add exciting ASCII art option to the tethys version command

--- tests/unit_tests/test_tethys_cli/test_version_command.py
+++ tests/unit_tests/test_tethys_cli/test_version_command.py
@@ -20,13 +20,25 @@ class VersionCommandTests(unittest.TestCase):
             "version", help="Print the version of tethys_platform"
         )
         mock_subparsers.add_parser().set_defaults.assert_called_with(
-            func=vc.version_command
+            func=vc.version_command, exciting=False
         )
 
-    @mock.patch("builtins.print")
+    @mock.patch("tethys_cli.version_command.print")
     def test_version_command(self, mock_print):
-        import tethys_portal
+        from tethys_portal import __version__
 
-        mock_args = mock.MagicMock()
+        mock_args = mock.MagicMock(exciting=False)
         vc.version_command(mock_args)
-        mock_print.assert_called_with(tethys_portal.__version__)
+        mock_print.assert_called_with(__version__)
+
+    @mock.patch("tethys_cli.version_command.Figlet")
+    @mock.patch("tethys_cli.version_command.print")
+    def test_version_command_exciting(self, mock_print, mock_Figlet):
+        from tethys_portal import __version__
+
+        mock_args = mock.MagicMock(exciting=True)
+        vc.version_command(mock_args)
+        mock_Figlet.assert_called_with(font="standard", width=300)
+        mock_Figlet().renderText.assert_any_call("Tethys Platform")
+        mock_Figlet().renderText.assert_any_call(__version__)
+        mock_print.assert_called_with(mock_Figlet().renderText())
--- tethys_cli/version_command.py
+++ tethys_cli/version_command.py
@@ -1,3 +1,4 @@
+from pyfiglet import Figlet
 from tethys_portal import __version__
 
 
@@ -6,8 +7,20 @@ def add_version_parser(subparsers):
     version_parser = subparsers.add_parser(
         "version", help="Print the version of tethys_platform"
     )
-    version_parser.set_defaults(func=version_command)
+    version_parser.add_argument(
+        "-e",
+        "--exciting",
+        help="Print the version of Tethys Platform in a more exciting way.",
+        action="store_true",
+        dest="exciting",
+    )
+    version_parser.set_defaults(func=version_command, exciting=False)
 
 
 def version_command(args):
-    print(__version__)
+    if args.exciting:
+        f = Figlet(font="standard", width=300)
+        print(f.renderText("Tethys Platform"))
+        print(f.renderText(__version__))
+    else:
+        print(__version__)
